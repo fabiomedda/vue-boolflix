b579c253c1351afbf4dffc7caa8fdb221d084f64
fabiomedda <meddafabio@gmail.com>
2021-01-14 17:38:19 +0100

    milestone 4 in corso

--- milestone 4/assets/js/main.js
+++ milestone 4/assets/js/main.js
@@ -1,18 +1,15 @@
 /*
-Milestone 3:
-In questa milestone come prima cosa aggiungiamo la copertina 
-del film o della serie al nostro elenco.
-Ci viene passata dall’ API solo la parte finale dell’ URL, 
-questo perché poi potremo generare da quella porzione di URL 
-tante dimensioni diverse.
-Dovremo prendere quindi l’ URL base delle immagini di TMDB:
-https://image.tmdb.org/t/p/ 
-per poi aggiungere la dimensione che vogliamo generare
-(troviamo tutte le dimensioni possibili a questo link:
-https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400 ) 
-per poi aggiungere la
-parte finale dell’ URL passata dall’ API.Esempio di URL:
-https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+Milestone 4:
+Trasformiamo quello che abbiamo fatto fino ad ora in una 
+vera e propria webapp, creando un layout completo 
+simil - Netflix: 
+●Un header che contiene logo e search bar
+● Dopo aver ricercato qualcosa nella searchbar, 
+i risultati appaiono sotto forma
+di“ card” in cui lo sfondo è rappresentato dall’ immagine 
+di copertina(consiglio la poster_path con w342)
+● Andando con il mouse sopra una card(on hover), appaiono le informazioni
+aggiuntive già prese nei punti precedenti più la overview
 */
 
 let root = new Vue ({
@@ -25,6 +22,7 @@ let root = new Vue ({
         pageSerieTV: 1,
         totalPageFilms: null,
         totalPageSerieTV: null,
+        flags: ["it", "en", "es", "fr"],
     },
     methods: {
         functionSearch(){
